a40d1a6622d1060ac196144e756218c8dd19ab92
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2017-01-05 18:07:07 +0100

    continuate lezioni

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,13 @@ import { Recipe } from './../recipe';
   styles: []
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', []),
+    new Recipe('Spaghetti', 'A classic', 'http://www.gushmag.it/wp-content/uploads/2015/03/pasta1.jpg', []),
+    new Recipe('Sauerkraut', 'Really manly recipe', 'http://lh6.ggpht.com/_2yL50eRSq1o/SxJyjZfKGKI/AAAAAAAAGH0/JnHz-q9mY0Q/s800/Crauti.JPG', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe: Recipe = new Recipe('dummy', 'dummy', 'http://www.gushmag.it/wp-content/uploads/2015/03/pasta1.jpg')
   
   constructor() { }
 
